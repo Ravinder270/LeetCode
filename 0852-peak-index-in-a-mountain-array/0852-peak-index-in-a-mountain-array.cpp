ebee2e98e6e5c7310694cade021ec13d43690e03
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-01-02 18:46:47 +0530

    Time: 126 ms (78.79%), Space: 62.3 MB (21.33%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int> A) {
+        for (int i = 1; i + 1 < A.size(); ++i) if (A[i] > A[i + 1]) return i;
+        return 0;
+    }
+};
