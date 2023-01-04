533bc4529584f7995a6827e9002f94d3d6673d6e
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-01-04 13:14:19 +0530

    Time: 86 ms (35.02%), Space: 19 MB (72.68%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int len=intervals.size();
+        if(len<=1)
+        {
+            return intervals;
+        }
+        
+        vector<vector<int>>res;
+        sort(intervals.begin(),intervals.end());
+        
+        res.push_back(intervals[0]);
+        for(int i=1;i<len;i++)
+        {
+            if(res.back()[1]>=intervals[i][0])
+            {
+                res.back()[1]=max(res.back()[1],intervals[i][1]);
+            }
+            else 
+            {
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+            
+    }
+};
