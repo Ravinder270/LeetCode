205645e23070cfde192ff24cf6792a18783f5dd0
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-01-21 00:29:54 +0530

    Time: 69 ms (73.56%), Space: 25.5 MB (53.37%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-   vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> seq;
-        dfs(res, seq, nums, 0);
-        return res;
+    vector<vector<int>>ans;
+   vector<vector<int>> findSubsequences(vector<int>& nums){
+    vector<int> temp;
+    dfs(nums,temp,0);
+       return ans;
     }
     
-    void dfs(vector<vector<int>>& res, vector<int>& seq, vector<int>& nums, int pos) {
-        if(seq.size() > 1) res.push_back(seq);
-        unordered_set<int> hash;
-        for(int i = pos; i < nums.size(); ++i) {
-            if((seq.empty() || nums[i] >= seq.back()) && hash.find(nums[i]) == hash.end()) {
-                seq.push_back(nums[i]);
-                dfs(res, seq, nums, i + 1);
-                seq.pop_back();
-                hash.insert(nums[i]);
-            }
-        }
-    }
+   void dfs(vector<int>&nums,vector<int>&temp,int index)
+   {
+       if(temp.size()>1)ans.push_back(temp);
+       
+       unordered_set<int>s;
+       for(int i=index;i<nums.size();i++)
+       {
+           if((temp.size()==0 || temp.back()<=nums[i]) && (s.find(nums[i])==s.end()))
+           {
+               temp.push_back(nums[i]);
+               dfs(nums,temp,i+1);
+               temp.pop_back();
+               s.insert(nums[i]);
+           }
+       }
+       
+       
+   }
+    
 };
