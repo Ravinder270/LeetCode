79f5e3212967d0be43c9078acd17fe88f2070951
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-02-09 08:02:39 +0530

    Time: 507 ms (21.60%), Space: 28 MB (6.87%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int n = size(nums), i = 0, maxReachable = 0, lastJumpedPos = 0, jumps = 0;
-	while(lastJumpedPos < n - 1) {  // loop till last jump hasn't taken us till the end
-		maxReachable = max(maxReachable, i + nums[i]);  // furthest index reachable on the next level from current level
-		if(i == lastJumpedPos) {			  // current level has been iterated & maxReachable position on next level has been finalised
-			lastJumpedPos = maxReachable;     // so just move to that maxReachable position
-			jumps++;                          // and increment the level
-	// NOTE: jump^ only gets updated after we iterate all possible jumps from previous level
-	//       This ensures jumps will only store minimum jump required to reach lastJumpedPos
-		}            
-		i++;
-	}
-	return jumps;
+    vector<int>dp;
+    
+    int recurs(vector<int>& nums,int i)
+    {
+        if(i>=nums.size())
+            return 10000000;
+        
+        if(i==nums.size()-1)
+            return 0;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int a=10000000;
+        
+         for(int k=1;k<=nums[i]&&k+i<nums.size();k++)
+         {
+               a=min(a,recurs(nums,k+i)+(1));
+         }
+           
+        return dp[i]=a;
     }
+    
+    
+    int jump(vector<int>& nums) {
+            dp.resize(20001,-1);
+        return recurs(nums,0);
+    }
+
 };
