08eb65eb00d066ec7b93b28c6292ca793fe34187
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-03-01 12:19:51 +0530

    Time: 7 ms (69.89%), Space: 10.2 MB (55.62%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int>s(nums1.begin(),nums1.end());
-        set<int>ans;
-        for(int i=0;i<nums2.size();i++)
+        int i=0;
+        int j=0;
+        
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        set<int>s;
+        
+        while(i<nums1.size() && j<nums2.size())
         {
-           if(s.find(nums2[i])!=s.end())
-           {
-               ans.insert(nums2[i]);
-           }
-            
+            if(nums1[i]==nums2[j])
+            {
+                s.insert(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
         }
         
-        vector<int>res;
-        for(auto it:ans)
+        vector<int>ans;
+        for(auto it:s)
         {
-            res.push_back(it);
+            ans.push_back(it);
         }
-        return res;
+        return ans;
+        
     }
 };
