ea79b516d61eb7f2c6a60ebd444fc6c445488b13
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-03-13 16:46:17 +0530

    Time: 5 ms (58.39%), Space: 16.3 MB (96.56%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,20 +11,21 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* a,TreeNode* b)
-
- {
-
-     if(a==NULL||b==NULL) return (a==b);
-
-     if(a->val!=b->val) return false;
-
-     return solve(a->left,b->right)&&solve(a->right,b->left);
-
- }
-
-    bool isSymmetric(TreeNode* A) {
-     
-       return solve(A->left,A->right);
+   bool isSymmetric(TreeNode* root) {
+        // Special case...
+        if(root == nullptr) return true;
+        // Return the function recursively...
+        return isSymmetric(root->left,root->right);
+    }
+    // A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
+    bool isSymmetric(TreeNode* leftroot,TreeNode* rightroot){
+        // If both root nodes are null pointers, return true...
+        if(!leftroot && !rightroot)  return true;
+        // If exactly one of them is a null node, return false...
+        if(!leftroot || !rightroot)  return false;
+        // If root nodes haven't same value, return false...
+        if(leftroot->val != rightroot->val)  return false;
+        // Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
+        return isSymmetric(leftroot->left, rightroot->right) && isSymmetric(leftroot->right, rightroot->left);
     }
 };
