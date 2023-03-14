f2142b0d7cfd6af29aa1410be935d2e8403bc1ab
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-03-14 07:56:07 +0530

    Time: 29 ms (99.30%), Space: 16 MB (52.10%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    static bool myfunction(vector<int>& a, vector<int>& b){
-        return a[1] > b[1];
-    }
-    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-	    //custom sort (in increasing order of numberOfUnitsPerBox as we have to return  maximum total number of units )
-        sort(boxTypes.begin(),boxTypes.end(),myfunction);
-		//greedily pick boxes till capacity is full
-        int ans=0;
-        for(auto box: boxTypes){
-            int x=min(box[0],truckSize);  //choose minimum boxes from available boxes and capacity left
-            ans+=(x*box[1]);  //adding units in ans
-            truckSize-=x;  //reduce the capacity
-            if(!truckSize) break;  //capacity full
+     int maximumUnits(vector<vector<int>>& boxes, int truckSize) {
+        // support variables
+        int res = 0, sizeBucket[1001] = {}, maxBucket = INT_MIN, minBucket = INT_MAX;
+        // bucket sorting tthe boxes and recording the bucket range
+        for (auto &boxType: boxes) {
+            maxBucket = max(maxBucket, boxType[1]);
+            minBucket = min(minBucket, boxType[1]);
+            sizeBucket[boxType[1]] += boxType[0];
         }
-        return ans;
+		// carrying as many larger sized boxes as we can first
+        for (int i = maxBucket, size, currBatch; i >= minBucket; i--) {
+            size = sizeBucket[i];
+            if (!size) continue;
+            currBatch = min(size, truckSize);
+            truckSize -= currBatch;
+            res += currBatch * i;
+            if (!truckSize) break;
+        }
+        return res;
     }
 };
