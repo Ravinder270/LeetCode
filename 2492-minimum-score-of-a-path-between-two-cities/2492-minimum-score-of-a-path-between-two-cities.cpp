e66db8679990993b09b415bab1d3bfc549f43982
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-03-22 17:09:44 +0530

    Time: 461 ms (80.62%), Space: 123.5 MB (84.92%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -3,40 +3,45 @@ class Solution
 public:
     int minScore(int n, vector<vector<int>>& roads) 
     {
-        //create adjacency list of roads to make it a graph
-        vector<pair<int, int>> adj[n+1];
-        for(int i=0; i<roads.size(); i++)
-        {
-            adj[roads[i][0]].push_back({roads[i][1], roads[i][2]});
-            adj[roads[i][1]].push_back({roads[i][0], roads[i][2]});
-        }
+       //Creating adjaceny list
+        vector<pair<int,int>>adj[n+1];
         
-        //mark possible visitable nodes 
-        vector<int> vis(n+1, 0);
-        queue<int> q;
-        q.push(1), vis[1]=1;
-        while(!q.empty())
+        for(int i=0;i<roads.size();i++)
         {
-            int node=q.front();
-            q.pop();
-            for(auto it: adj[node])
-            {
-                if(!vis[it.first])
-                {
-                    q.push(it.first);
-                    vis[it.first]=1;
-                }
-            }
+            adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
+            adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
         }
-            
-        //find minimum path score among visited nodes
-        int mini_path = INT_MAX;
-        for(int i=0; i<roads.size(); i++)
-        {
-            if(vis[roads[i][0]] && vis[roads[i][1]] && roads[i][2] < mini_path)
-                mini_path = roads[i][2];
-        }
-        return mini_path;
-        
+      
+        //performing BFS
+   vector<int>vis(n+1,0);
+    queue<int> q;
+     q.push(1);
+     vis[1]=1;
+     while(!q.empty())
+     {
+         int node=q.front();
+         q.pop();
+         
+         for(auto it:adj[node])
+         {
+             if(!vis[it.first])
+             {
+                 vis[it.first]=1;
+                 q.push(it.first);
+             }
+         }
+     }
+     
+     //we traversed whole graph 
+        //now we will be finding minimum path
+        int mini=INT_MAX;
+      for(int i=0;i<roads.size();i++)
+      {
+          if(vis[roads[i][0]] && vis[roads[i][1]] && roads[i][2]<mini)
+                                     {
+                                         mini=roads[i][2];
+                                     }
+      }
+        return mini;
     }
 };
