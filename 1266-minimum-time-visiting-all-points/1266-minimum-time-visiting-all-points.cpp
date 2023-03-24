99784d34e0c1f3b0d1d056b0e49b55f9c5ddce2d
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-03-24 19:43:17 +0530

    Time: 16 ms (7.75%), Space: 10.2 MB (27.40%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
+    //Brute force approach
     int minTimeToVisitAllPoints(vector<vector<int>>& A) {
+     int cur_X=A[0][0];
+     int cur_Y=A[0][1];
      
-         int n=A.size();
- //       int m=A[0].size();
- int res=0;
- 
-for(int i=1;i<n;i++)
-{
-    
-        res+=max(abs(A[i][0]-A[i-1][0]),abs(A[i][1]-A[i-1][1]));
-}
- return res;
-        
+      int count=0;  
+     for(int i=1;i<A.size();i++)
+     {
+         int destX=A[i][0];
+         int destY=A[i][1];
+         while(cur_X!=destX ||cur_Y!=destY)
+         {
+             if(cur_X<destX)cur_X++;
+             else if(cur_X>destX)cur_X--;
              
+             if(cur_Y<destY)cur_Y++;
+             else if(cur_Y>destY)cur_Y--;
+             
+             count++;
+         }
+     }
+        return count;
+        
     }
 };
