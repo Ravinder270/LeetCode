1e3fd81bf94ea70cbb3ae5fa6298fb9edbf2c0a1
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-03-24 19:25:51 +0530

    Time: 9 ms (59.72%), Space: 10.2 MB (74.34%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int sum=0;
-	        for(int i=0;i<points.size()-1;i++)
-	        {
-		         sum+=max(abs(points[i][0]-points[i+1][0]),abs(points[i][1]-points[i+1][1]));// Diagonal traversal
-	         }
-	         return sum;
+    int minTimeToVisitAllPoints(vector<vector<int>>& A) {
+     
+         int n=A.size();
+ //       int m=A[0].size();
+ int res=0;
+ 
+for(int i=1;i<n;i++)
+{
+    
+        res+=max(abs(A[i][0]-A[i-1][0]),abs(A[i][1]-A[i-1][1]));
+}
+ return res;
+        
              
     }
 };
