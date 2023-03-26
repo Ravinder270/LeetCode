10af250fa3e4c8198103da4bba58a565f9b0ba99
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-03-27 05:27:50 +0530

    Time: 224 ms (94.94%), Space: 99 MB (61.11%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& A, long k){
-        int i = 0, j;
-        sort(A.begin(), A.end());
-        for (j = 0; j < A.size(); ++j) {
-            k += A[j];
-            if (k < (long)A[j] * (j - i + 1))
-                k -= A[i++];
+        
+        int i=0;
+        int j;
+        sort(A.begin(),A.end());
+        for(j=0;j<A.size();j++)
+        {
+            k+=A[j];
+            if(k   <   (long)A[j]*(j-i+1))
+            {
+                k-=A[i++];
+            }
         }
-        return j - i;
+        
+        return j-i;
     
     }
 };
