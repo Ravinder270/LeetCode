5bdac963a4c9b5ec3245a27679808fbb389f4213
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-03-27 02:48:03 +0530

    Time: 631 ms (23.11%), Space: 193.9 MB (23.53%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,19 +1,57 @@
 class Solution {
 public:
-    int longestCycle(vector<int>& e) {
-    int res = -1;
-    vector<pair<int, int>> memo(e.size(), {-1, -1});
-    for (int i = 0; i < e.size(); ++i)
-        for (int j = i, dist = 0; j != -1; j = e[j]) {
-            auto [dist_i, from_i] = memo[j];
-            if (dist_i == -1)
-                memo[j] = {dist++, i};
-            else {
-                if (from_i == i)
-                    res = max(res, dist - dist_i);
-                break;
+    stack <int> s;
+	void dfs(vector <int> adj[], vector <int> &vis, int root){
+	    vis[root] = 1;
+	    for(auto &i:adj[root]){
+	        if(!vis[i])
+	            dfs(adj, vis, i);
+	    }
+	    s.push(root);
+	}
+	void dfs1(vector <int> adj[], vector <int> &vis, int root, int level, int &ans){
+        ans = max(ans, level);
+	    vis[root] = 1;
+	    for(auto &i:adj[root]){
+	        if(!vis[i])
+	            dfs1(adj, vis, i, level+1, ans);
+	    }
+	}
+    int kosaraju(int V, vector<int> adj[])
+    {
+        vector <int> vis(V);
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                dfs(adj, vis, i);
             }
         }
-    return res;
-}
+        vector <int> vec[V];
+        for(int i=0; i<V; i++){
+            for(auto &j:adj[i]){
+                vec[j].push_back(i);
+            }
+        }
+        fill(vis.begin(), vis.end(), 0);
+        int c = 0;
+        while(!s.empty()){
+            int t = s.top();
+            s.pop();
+            if(!vis[t]){
+                int ans = 0;
+                dfs1(vec, vis, t, 1, ans);
+                c = max(c, ans);
+            }
+        }
+        return c;
+    }
+    int longestCycle(vector<int>& edges) {
+        int V = edges.size();
+        vector <int> adj[V];
+        for(int i=0; i<V; i++){
+            if(edges[i] != -1)
+                adj[i].push_back(edges[i]);
+        }
+        int ans = kosaraju(V, adj);
+        return ans == 1 ? -1 : ans;
+    }
 };
