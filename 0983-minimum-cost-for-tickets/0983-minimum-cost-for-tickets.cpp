bc5c8a2c81e0d6304e4ea1c665379397e92b9ac8
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-03-29 01:43:10 +0530

    Time: 4 ms (69.83%), Space: 9.6 MB (57.50%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    //1.Recursion
-    int solve(int n,int index,vector<int>&days,vector<int>&cost,vector<int>&dp)
+    //1.Tabulation
+    int solve(int n,vector<int>&days,vector<int>&cost)
     {
-        //Base Case
-        if(index>=n)return 0;
+        vector<int>dp(n+1,INT_MAX);
         
-        if(dp[index]!=-1)return dp[index];
-        //1-day cost
-        int option1= cost[0]+solve(n,index+1,days,cost,dp);
+        dp[n]=0;
+        
+        for(int k=n-1;k>=0;k--)
+        {
+            //1-day cost
+            int option1= cost[0]+dp[k+1];
         
         //2-day cost
         //1.Take index to the desired location
         int i;
-        for(i=index;i<n && days[i]<days[index]+7;i++);
+        for(i=k;i<n && days[i]<days[k]+7;i++);
         
         //main thing
-        int option2=cost[1]+ solve(n,i,days,cost,dp);
+        int option2=cost[1]+ dp[i];
         
         //option 3
-        for(i=index;i<n && days[i]<days[index]+30;i++);
+        for(i=k;i<n && days[i]<days[k]+30;i++);
         
-        int option3=cost[2] + solve(n,i,days,cost,dp);
+        int option3=cost[2] + dp[i];
         
         
-        dp[index]=min(option1,min(option2,option3));
+        dp[k]=min(option1,min(option2,option3));
         
-        return dp[index];
+        }
+        
+        
+        return dp[0];
     }
     int mincostTickets(vector<int>& days, vector<int>& cost) {
-        vector<int>dp(days.size(),-1);
-        return solve(days.size(),0,days,cost,dp);
+        //vector<int>dp(days.size(),-1);
+        return solve(days.size(),days,cost);
     }
 };
