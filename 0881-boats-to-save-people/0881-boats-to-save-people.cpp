c0638438a2078fdf3b9db64fc3f04883963e38f0
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-04-03 07:57:34 +0530

    Time: 94 ms (33.96%), Space: 42.1 MB (11.29%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,35 @@
+// 1. sort the people array
+// 2. set i =0 and j = n-1
+// 3. traverse and check the sum of people[i] and people[j] is less than or equal to limit 
+// if true {
+//     count++;
+//     j--;
+//     i++;
+// }
+// else{
+// //assign boat to j and reduce j
+//     count++; j--;
+// }
+// TC - O(n logn)
+// SC - O(1)
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int n = people.size();
+        int ans=0;
+        int i=0,j = n-1;
+        while(i<=j){
+            if(people[i] + people[j] <= limit){
+                ans++;
+				i++;  j--;
+            }
+            else{
+                ans++;
+				j--;
+            }
+        }
+        return ans;
+    }
+};
