bfb5d648a4aee04104d56a1d5e4316f57522d15a
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-04-03 22:40:51 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (43.84%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     
-    bool prefixsum(vector<int>&nums,int startvalue)
-    {
-        int sum=startvalue;
-        bool flag=true;
-        for(int i=0;i<nums.size();i++)
-        {
-            sum+=nums[i];
-            if(sum<1)
-            {
-                flag=false;
-                break;
-            }
-        }
-        return flag;
-    }
+   
     int minStartValue(vector<int>& nums) {
-        
-        int startvalue=0;
-        for(int i=1;i<=3000;i++)
+    
+        //minimum running sum
+        int running_sum=0;
+        int sum=0;
+        for(auto val:nums)
         {
-            if(prefixsum(nums,i))
-            {
-                startvalue=i;
-                break;
-            }
+            sum+=val;
+            running_sum=min(sum,running_sum);
         }
-        return startvalue;
+        return 1- running_sum;
+        
     }
 };
