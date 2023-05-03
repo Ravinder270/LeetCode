0bb6dfee5cf4aa12b41d2d357e6958bb426efe30
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-05-04 00:02:21 +0530

    Time: 78 ms (11.57%), Space: 42.7 MB (21.04%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int maxScore(vector<int>& A, int B) {
-        int n=A.size();
- int suff[n+1];
- suff[n]=0;
- suff[n-1]=A[n-1];
- for(int fill=n-2;fill>=0;fill--)
- {
-     suff[fill]=A[fill]+suff[fill+1];
- }
- 
- int prefix=0;
- int ans=suff[n-B];
- for(int i=0;i<B;i++)
- {
-     prefix+=A[i];
-     int suffsum=suff[n-B+i+1];
-     ans=max(ans,prefix+suffsum);
- }
-  return ans;
+    int maxScore(vector<int>& A, int k) {
+       int n=A.size();
+       int suff[n+1];
+        suff[n]=0;
+        suff[n-1]=A[n-1];
+        //filling suffix array obviously from right
+        for(int i=n-2;i>=0;i--)
+        {
+            suff[i]=suff[i+1]+A[i];
+        }
+        
+        int ans=suff[n-k];
+        int prefix=0;
+        for(int i=0;i<k;i++)
+        {
+            prefix=prefix + A[i];
+            ans=max(ans,prefix+suff[n-k+(i+1)]);            
+        }
+        return ans;
     }
 };
