8ed0824cc04ed4a099deed27438ffaad5a20aafa
Ravinder Singh <107816753+Ravinder270@users.noreply.github.com>
2023-05-23 19:01:56 +0530

    Time: 8 ms (91.59%), Space: 5.9 MB (96.70%) - LeetHub

---
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,25 @@
+
+
+class Solution {
+public:
+    bool CanMake(int num, int target) {
+  if (target < 0 || num < target) {
+    return false;
+  }
+  if (num == target) {
+    return true;
+  }
+  return CanMake(num / 10, target - num % 10) || CanMake(num / 100, target - num % 100) ||
+      CanMake(num / 1000, target - num % 1000);
+}
+  int punishmentNumber(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n; ++i) {
+      int num = i * i;
+      if (CanMake(num, i)) {
+        answer += num;
+      }
+    }
+    return answer;
+  }
+};
